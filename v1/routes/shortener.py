53ea89429bf242d03c1a488a790b4f4455a6db41
Christopher Lewerenz <c.lewerenz@mail.de>
2022-07-09 00:22:52 +0200

    sorted code

--- v1/routes/shortener.py
+++ v1/routes/shortener.py
@@ -1,45 +1,26 @@
 from fastapi import APIRouter
-from core.database.crud import *
-from core.database.configuration import session
+
+
 from core.model.shortener import URL as UrlModel
 from core.schema.shortener import URL as UrlSchema, ShortUrl as ShortUrlSchema
 from fastapi.responses import RedirectResponse
-import random
-import string
+
+from v1.services.shortener import get_all_urls_from_db, get_url_from_shorty, shorten_url
+
 
 router = APIRouter()
 
 
 @router.get("/urls", response_model=list[UrlSchema])
-async def test_hello():
-    result = select_all(session, UrlModel)
-    return result
-
-
-def generate_and_check_shorty():
-    shorty = ''.join(random.choice(string.ascii_lowercase +
-                     string.digits) for _ in range(6))
-    if select_where(session, UrlModel, UrlModel.short_url == shorty):
-        return generate_and_check_shorty()
-    return shorty
+async def get_all_urls():
+    return get_all_urls_from_db()
 
 
 @router.post("/shorten")
-async def shorten_url(origin_url: str):
-
-    check_origin = select_where(
-        session, UrlModel, UrlModel.origin_url == origin_url)
-    if check_origin:
-        return check_origin[0].short_url
-
-    random_shorty = generate_and_check_shorty()
-
-    url_object = UrlModel(origin_url=origin_url, short_url=random_shorty)
-    result = insert_one(session, UrlModel, url_object)
-    return random_shorty
+async def short_url(origin_url: str):
+    return "http://127.0.0.1:8000/" + str(shorten_url(origin_url))
 
 
 @router.get("/{shorty}")
 async def redirect(shorty):
-    query = select_where(session, UrlModel, UrlModel.short_url == shorty)
-    return RedirectResponse(url=query[0].origin_url)
+    return RedirectResponse(url=get_url_from_shorty(shorty))
--- v1/services/shortener.py
+++ v1/services/shortener.py
@@ -1,2 +1,32 @@
-def shorten_url(url: str):
-    pass
+from core.database.crud import *
+from core.model.shortener import URL as UrlModel
+from core.database.configuration import session
+import random
+import string
+
+
+def generate_and_check_shorty():
+    shorty = ''.join(random.choice(string.ascii_lowercase +
+                     string.digits) for _ in range(6))
+    if select_where(session, UrlModel, UrlModel.short_url == shorty):
+        return generate_and_check_shorty()
+    return shorty
+
+def get_all_urls_from_db():
+    return select_all(session, UrlModel)
+
+def shorten_url(origin_url: str):
+    check_origin = select_where(
+        session, UrlModel, UrlModel.origin_url == origin_url)
+    if check_origin:
+        return check_origin[0].short_url
+
+    random_shorty = generate_and_check_shorty()
+
+    url_object = UrlModel(origin_url=origin_url, short_url=random_shorty)
+    result = insert_one(session, UrlModel, url_object)
+    return random_shorty
+
+def get_url_from_shorty(shorty: str):
+    query = select_where(session, UrlModel, UrlModel.short_url == shorty)
+    return query[0].origin_url 
